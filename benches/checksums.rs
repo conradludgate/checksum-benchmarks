a11278f8798d232afdf234ee9a8ef4dce68628fa
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-11-01 14:12:23 -0400

    Add benchmark throughput

--- benches/checksums.rs
+++ benches/checksums.rs
@@ -1,4 +1,5 @@
 use blake2::digest::{typenum::U4, Digest};
+use divan::{counter::BytesCount, Bencher};
 
 fn main() {
     // Run registered benchmarks.
@@ -17,49 +18,67 @@ const LARGE_PAGE: &[u8; 1 << 20] = &{
 };
 
 #[divan::bench(consts = SIZES)]
-fn crc<const N: usize>() -> u32 {
-    crc32fast::hash(&divan::black_box(LARGE_PAGE)[..N])
+fn crc<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crc32fast::hash(&divan::black_box(LARGE_PAGE)[..N]))
 }
 
 #[divan::bench(consts = SIZES)]
-fn adler<const N: usize>() -> u32 {
-    adler::adler32_slice(&divan::black_box(LARGE_PAGE)[..N])
+fn adler<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| adler::adler32_slice(&divan::black_box(LARGE_PAGE)[..N]))
 }
 
 #[divan::bench(consts = SIZES)]
-fn blake2b_32<const N: usize>() -> u32 {
-    crypto::<blake2::Blake2b<U4>, N>()
+fn blake2b_32<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<blake2::Blake2b<U4>, N>())
 }
 
 #[divan::bench(consts = SIZES)]
-fn blake2b_512<const N: usize>() -> u32 {
-    crypto::<blake2::Blake2b512, N>()
+fn blake2b_512<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<blake2::Blake2b512, N>())
 }
 
 #[divan::bench(consts = SIZES)]
-fn blake2s_32<const N: usize>() -> u32 {
-    crypto::<blake2::Blake2s<U4>, N>()
+fn blake2s_32<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<blake2::Blake2s<U4>, N>())
 }
 
 #[divan::bench(consts = SIZES)]
-fn blake2s_256<const N: usize>() -> u32 {
-    crypto::<blake2::Blake2s256, N>()
+fn blake2s_256<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<blake2::Blake2s256, N>())
 }
 
 #[divan::bench(consts = SIZES)]
-fn blake3<const N: usize>() -> u32 {
-    let res = blake3::hash(&divan::black_box(LARGE_PAGE)[..N]);
-    u32::from_ne_bytes(res.as_bytes()[0..4].try_into().unwrap())
+fn blake3<const N: usize>(bencher: Bencher) {
+    bencher.counter(BytesCount::new(N)).bench(|| {
+        let res = blake3::hash(&divan::black_box(LARGE_PAGE)[..N]);
+        u32::from_ne_bytes(res.as_bytes()[0..4].try_into().unwrap())
+    })
 }
 
 #[divan::bench(consts = SIZES)]
-fn sha1<const N: usize>() -> u32 {
-    crypto::<sha1::Sha1, N>()
+fn sha1<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<sha1::Sha1, N>())
 }
 
 #[divan::bench(consts = SIZES)]
-fn sha256<const N: usize>() -> u32 {
-    crypto::<sha2::Sha256, N>()
+fn sha256<const N: usize>(bencher: Bencher) {
+    bencher
+        .counter(BytesCount::new(N))
+        .bench(|| crypto::<sha2::Sha256, N>())
 }
 
 fn crypto<D: Digest, const N: usize>() -> u32 {
