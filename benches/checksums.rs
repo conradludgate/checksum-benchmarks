33e2582d4b0101650dba845fab15187b2885567b
Conrad Ludgate <conradludgate@gmail.com>
2023-11-21 13:55:25 +0000

    add more benchies

--- benches/checksums.rs
+++ benches/checksums.rs
@@ -37,15 +37,17 @@ mod non_crypto {
 
 #[divan::bench_group]
 mod crypto {
+    use blake2::digest::{generic_array::GenericArray, Digest};
+
     #[divan::bench_group]
     mod md {
         bench! {
             md5 => md5_impl;
         }
 
-        fn md5_impl(page: &[u8]) -> u32 {
+        fn md5_impl(page: &[u8]) -> u128 {
             let res = md5::compute(page);
-            u32::from_ne_bytes(res.0[0..4].try_into().unwrap())
+            u128::from_ne_bytes(res.0)
         }
     }
 
@@ -57,36 +59,36 @@ mod crypto {
             sha1 => crypto::<sha1::Sha1>;
             sha2_256 => crypto::<sha2::Sha256>;
             sha3_256 => crypto::<sha3::Sha3_256>;
+            sha2_384 => crypto::<sha2::Sha384>;
+            sha3_384 => crypto::<sha3::Sha3_384>;
+            sha2_512 => crypto::<sha2::Sha512>;
+            sha3_512 => crypto::<sha3::Sha3_512>;
         }
     }
 
     #[divan::bench_group]
     mod blake {
         use super::crypto;
-        use blake2::digest::typenum::U4;
 
         bench! {
-            blake2b_32 => crypto::<blake2::Blake2b<U4>>;
-            blake2b_512 => crypto::<blake2::Blake2b512>;
-            blake2s_32 => crypto::<blake2::Blake2s<U4>>;
-            blake2s_256 => crypto::<blake2::Blake2s256>;
+            blake2b => crypto::<blake2::Blake2b512>;
+            blake2s => crypto::<blake2::Blake2s256>;
             blake3_mt => blake3_mt_impl;
             blake3 => blake3_impl;
         }
 
-        fn blake3_mt_impl(page: &[u8]) -> u32 {
+        fn blake3_mt_impl(page: &[u8]) -> [u8; 32] {
             let res = blake3::Hasher::new().update_rayon(page).finalize();
-            u32::from_ne_bytes(res.as_bytes()[0..4].try_into().unwrap())
+            *res.as_bytes()
         }
 
-        fn blake3_impl(page: &[u8]) -> u32 {
+        fn blake3_impl(page: &[u8]) -> [u8; 32] {
             let res = blake3::Hasher::new().update(page).finalize();
-            u32::from_ne_bytes(res.as_bytes()[0..4].try_into().unwrap())
+            *res.as_bytes()
         }
     }
 
-    fn crypto<D: blake2::digest::Digest>(page: &[u8]) -> u32 {
-        let res = D::new().chain_update(page).finalize();
-        u32::from_ne_bytes(res[0..4].try_into().unwrap())
+    fn crypto<D: Digest>(page: &[u8]) -> GenericArray<u8, D::OutputSize> {
+        D::new().chain_update(page).finalize()
     }
 }
